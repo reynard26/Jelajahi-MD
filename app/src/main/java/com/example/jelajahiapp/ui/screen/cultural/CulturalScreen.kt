ab60a29eb34089f20e5fa76ae2c8e2cdf2ceb601
Reynardthan Handoko <79642115+reynard26@users.noreply.github.com>
2023-12-15 17:30:52 +0700

    Cultural

--- app/src/main/java/com/example/jelajahiapp/ui/screen/cultural/CulturalScreen.kt
+++ app/src/main/java/com/example/jelajahiapp/ui/screen/cultural/CulturalScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -36,7 +38,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,10 +46,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.jelajahiapp.R
 import com.example.jelajahiapp.component.ScrollToTopButton
-import com.example.jelajahiapp.data.ViewModelFactory
-import com.example.jelajahiapp.ui.screen.home.HomeViewModel
 import com.example.jelajahiapp.data.Result
+import com.example.jelajahiapp.data.ViewModelFactory
 import com.example.jelajahiapp.data.room.Cultural
+import com.example.jelajahiapp.ui.screen.home.HomeViewModel
 import com.example.jelajahiapp.ui.screen.home.truncate
 import com.example.jelajahiapp.ui.theme.fonts
 import com.example.jelajahiapp.ui.theme.purple100
@@ -111,7 +113,14 @@ fun CulturalContent(
                         )
                     }
                 },
-                title = {},
+                title = {
+                    Image(painter = painterResource(id = R.drawable.logo),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(100.dp)
+                            .padding(0.dp, 25.dp, 0.dp, 0.dp)
+                    )
+                },
             )
         }
     ) { innerPadding ->
